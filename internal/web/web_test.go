45e5c1c0d423af5baf9cefdbf3f3489be0515bc7
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-01-09 13:51:35 +0300

    cover RunWebServer()

--- internal/web/web_test.go
+++ internal/web/web_test.go
@@ -1,24 +1,15 @@
 package web
 
 import (
-	"fmt"
 	"testing"
 	"time"
-
-	"github.com/gorilla/mux"
-	"go.uber.org/zap"
 )
 
 func TestRunWebServer(t *testing.T) {
 	errChan := make(chan error, 1)
 
 	go func() {
-		router := mux.NewRouter()
-		webServer := initServer(router, fmt.Sprintf(":%d", opts.ServerPort),
-			time.Duration(int32(opts.WriteTimeoutSeconds))*time.Second,
-			time.Duration(int32(opts.ReadTimeoutSeconds))*time.Second, logger)
-		logger.Info("starting web server", zap.Int("port", opts.ServerPort))
-		errChan <- webServer.ListenAndServe()
+		errChan <- RunWebServer()
 	}()
 
 	for {
